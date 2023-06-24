bace11647e98c75d5cfed881cdb6bdbfacb3e4f9
Suriya-K7 <usraising@gmail.com>
2023-06-24 15:57:37 +0530

    dashboard updated

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -14,27 +14,33 @@ const Dashboard = () => {
           return <Card key={index} data={data} />;
         })}
       </div>
-      <h2>Records</h2>
-      <table className="table table-hover table-striped table-success text-dark">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Batch</th>
-            <th scope="col">Mentor</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map((data, index) => (
-            <tr key={index}>
-              <th scope="row">{data.id}</th>
-              <td>{data.name}</td>
-              <td>{data.course}</td>
-              <td>{data.mentor}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h2 className="text-center text-gray-800">Records</h2>
+      {students.length !== 0 ? (
+        <>
+          <table className="table table-hover table-primary">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Batch</th>
+                <th scope="col">Mentor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((data, index) => (
+                <tr key={index}>
+                  <th scope="row">{data.id}</th>
+                  <td>{data.name}</td>
+                  <td>{data.course}</td>
+                  <td>{data.mentor}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      ) : (
+        <p className="text-center h3 text-muted">No students available</p>
+      )}
     </>
   );
 };
