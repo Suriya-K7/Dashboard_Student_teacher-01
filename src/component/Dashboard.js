8e510d1b138a6713c7498886ff362186a5ab3e89
Suriya-K7 <usraising@gmail.com>
2023-06-24 13:35:16 +0530

    student list aded in dashboard

--- src/component/Dashboard.js
+++ src/component/Dashboard.js
@@ -3,7 +3,7 @@ import Card from "./Card";
 import DataContext from "../Context/DataContext";
 
 const Dashboard = () => {
-  const { data } = useContext(DataContext);
+  const { data, students } = useContext(DataContext);
   return (
     <>
       <div className="d-sm-flex align-items-center justify-content-center mb-4">
@@ -14,6 +14,27 @@ const Dashboard = () => {
           return <Card key={index} data={data} />;
         })}
       </div>
+      <h2>Records</h2>
+      <table className="table table-hover table-striped table-success text-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Batch</th>
+            <th scope="col">Mentor</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((data, index) => (
+            <tr key={index}>
+              <th scope="row">{data.id}</th>
+              <td>{data.name}</td>
+              <td>{data.course}</td>
+              <td>{data.mentor}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
